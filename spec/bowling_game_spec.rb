72cd097ddef0fee03014eb7a313f54f72ac731ec
GeneChatham <genechatham@gmail.com>
2013-11-21 15:49:33 -0600

    Test can now detect spares and passes.

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -4,27 +4,39 @@ require 'rspec'
 require_relative '../bowling_game.rb'
 
 describe BowlingGame do
+	let(:game) {BowlingGame.new}
+
   describe '.initialize' do
     it 'sets @rolls to an empty array' do
-      game = BowlingGame.new
 
       expect(game.rolls).to eq([])
     end
   end
 
   describe '#score' do
-  	it 'scores the game as 0 when only gutter balls are thrown' do
-   		game = BowlingGame.new
-    	20.times { game.roll(0) }
+  	context 'when only gutter balls are thrown' do
+	  	it 'scores the game as 0 when only gutter balls are thrown' do
+	    	20.times { game.roll(0) }
 
-    	expect(game.score).to eq(0)
-  	end
-  	it 'scores the game as 20 when one pin is knocked down on each roll' do
-  	  game = BowlingGame.new
-  	  20.times { game.roll(1) }
+	    	expect(game.score).to eq(0)
+	  	end
+	  end
 
-  	  expect(game.score).to eq(20)
-  	end
-  	
+  	context 'when one pin is knocked down on each roll' do
+	  	it 'scores the game as 20 when one pin is knocked down on each roll' do
+	  	  20.times { game.roll(1) }
+
+	  	  expect(game.score).to eq(20)
+	  	end
+		end
+
+		context 'when a spare is thrown' do
+		  it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
+		    3.times  { game.roll(5) }
+		    17.times { game.roll(0) }
+
+		    expect(game.score).to eq(20)
+		  end
+		end
 	end
 end
