6142eb3e74c44f23d9517c54b0b32075d0f2f13a
Kendra McClanahan <kendramcclanahan@kendras-mbp.att.net>
2013-11-21 15:14:22 -0600

    All tests pass for gutter-ball game

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -10,5 +10,13 @@ describe BowlingGame do
 
       expect(game.rolls).to eq([])
     end
- end
+  end
+  describe '#score' do
+    it 'scores the game as 0 when only gutter balls are thrown' do
+      game = BowlingGame.new
+      20.times { game.roll(0) }
+
+      expect(game.score).to eq(0)
+    end
+  end
 end
